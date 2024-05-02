326627571c1c921df842f4a8bc8d7055022b5890
Todd Humphreys <todd.humphreys@utexas.edu>
2024-05-02 14:37:20 -0500

    sped up visualization

--- src/ros/view/view_manager.cc
+++ src/ros/view/view_manager.cc
@@ -58,7 +58,7 @@ void ViewManager::RunQuadPublisher(const QuadViewOptions quad_view_options) {
         quads_publisher->Publish(marker);
       }
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
   }
 }
 
