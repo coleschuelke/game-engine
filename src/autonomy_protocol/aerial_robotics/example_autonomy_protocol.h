fadcf0e40934d6756fabf8cd638687cea60dd932
Todd Humphreys <todd.humphreys@utexas.edu>
2025-04-09 11:00:08 -0500

    wip

--- src/autonomy_protocol/aerial_robotics/example_autonomy_protocol.h
+++ src/autonomy_protocol/aerial_robotics/example_autonomy_protocol.h
@@ -12,15 +12,13 @@
 namespace game_engine {
 
 // ExampleAutonomyProtocol is a class that demonstrates how to create a
-// trajectory for a quadcopter to follow. 
+// trajectory for a quadcopter to follow.
 class ExampleAutonomyProtocol : public AutonomyProtocol {
  public:
-  
   // This is the class constructor. It runs whenever a new object of type
   // ExampleAutonomyProtocol is created. It receives a set of input parameters
-  // and uses these to initialize some other objects. In our case, we initialize
-  // the ExampleAutonomyProtocol's parent class--AutonomyProtocol--as well as
-  // the occupancy grid and arena graph. 
+  // and uses these for initialization. In particular, it initializes its parent
+  // class, AutonomyProtocol, as well as the occupancy grid and arena graph.
   ExampleAutonomyProtocol(
       const std::vector<std::string>& friendly_names,
       const std::vector<std::string>& enemy_names,
@@ -40,19 +38,19 @@ class ExampleAutonomyProtocol : public AutonomyProtocol {
                          blue_balloon_status, blue_balloon_position,
                          goal_position, wind_intensity, visualizer) {
     occupancy_grid_.LoadFromMap(map3d_, cell_size_, safety_margin_);
-    
+
     // Once you have built a 3D version of A*, you can run it on graph_of_arena.
-    // Important hint: graph_of_arena will be large (many nodes) if cell_size is
-    // small.  If you naively extend your AStar2D function to 3D, it will run
-    // slowly on a large graph_of_arena.  Of course, you can keep the graph
-    // small by choosing a large cell_size, but this may prevent you from
-    // finding paths through areas with tight obstacle spacing.  A better
-    // approach is to speed up A*.  Note that its most costly operation is the
-    // search through the container of explored nodes to determine whether a
-    // candidate node has already been explored.  You can speed up this search
-    // dramatically by making this container an std::unordered_set rather than
-    // an std::vector.  See
-    // https://www.educative.io/edpresso/unordered-sets-in-cpp. 
+    // Important hint: graph_of_arena will be large (many nodes) if cell_size_
+    // (defined below) is small.  If you naively extend your AStar2D function to
+    // 3D, it will run slowly on a large graph_of_arena.  Of course, you can
+    // keep the graph small by choosing a large cell_size, but this may prevent
+    // you from finding paths through areas with tight obstacle spacing.  A
+    // better approach is to speed up A*.  Note that its most costly operation
+    // is the search through the container of explored nodes to determine
+    // whether a candidate node has already been explored.  You can speed up
+    // this search dramatically by making this container an std::unordered_set
+    // rather than an std::vector.  See
+    // https://how.dev/answers/unordered-sets-in-cpp
 
     graph_of_arena_ = occupancy_grid_.AsGraph();
   }
@@ -60,27 +58,25 @@ class ExampleAutonomyProtocol : public AutonomyProtocol {
   std::unordered_map<std::string, Trajectory> UpdateTrajectories() override;
 
  private:
-
   // The OccupancyGrid3D class, which has already been written for you, divides
   // the arena space into cells of a size you specify in cell_size below.  It
   // has convenient functions for determining whether a cell is occupied and for
-  // translating between a cell's grid index triple and its 3d cell center
+  // translating between a cell's grid index triple and its 3D cell center
   // position, in meters.  Have a look at its header file
-  // game-engine/src/environment/occupancy_grid3d.h to learn more about this
-  // class's API. 
+  // game-engine/src/environment/occupancy_grid3d.h to learn more about its API.
   OccupancyGrid3D occupancy_grid_;
   Graph3D graph_of_arena_;
 
-  
   // AutonomyProtocolVisualizer is a class that supports visualizing paths,
-  // curves, points, and whole trajectories in the RVIZ display of the arena to
-  // aid in your algorithm development.  Have a look at
-  // autonomy_protocol_visualizer.h to see this class's API. 
+  // curves, points, and whole trajectories in the RVIZ display of the arena,
+  // which will aid your algorithm development.  Have a look at
+  // autonomy_protocol_visualizer.h to see its API.
   AutonomyProtocolVisualizer visualizer_;
-  
-  // The length of one side of the occupancy grid's cubic cells, in meters
+
+  // Length of one side of the occupancy grid's cubic cells, in meters
   static constexpr double cell_size_ = 0.2;
-  // Length by which obstacles are inflated to provide a safety margin
+  // Length by which obstacles are inflated to provide a safety margin, in
+  // meters
   double safety_margin_ = 0.35;
   bool first_time_ = true;
   bool halt_ = false;
